74d6ecd7ba80dfee1068cba773a68499ff1292ef
Wataru Sugiura <38929316+flat35hd99@users.noreply.github.com>
2021-10-30 19:49:11 +0000

    Add gitignore

--- symmetryshift/setup.py
+++ symmetryshift/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name="symmetryshift",
-      version="0.0.3",
+      version="0.1.0",
       description="Create biological strucre unit from single PDB file",
       author="flat35hd99",
       packages=find_packages(),
