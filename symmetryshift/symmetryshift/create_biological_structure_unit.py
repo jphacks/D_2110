633cb32ff757c6d985b041dda1f59c3fa637725e
flat35hd99 <38929316+flat35hd99@users.noreply.github.com>
2021-10-31 03:06:17 +0900

    Add comment

--- symmetryshift/symmetryshift/create_biological_structure_unit.py
+++ symmetryshift/symmetryshift/create_biological_structure_unit.py
@@ -39,26 +39,47 @@ def creater(pdb_id: str):
     parser = PDBParser(QUIET=True)
     structure = parser.get_structure(pdb_id, pdb_file)
     models = list(structure.get_models())
-    model = models[0]
+    model = models[0] # Because single PDB file should contain single model, we get first one. 
     chains = list(structure.get_chains())
 
     # Work operator
     chain_ids_to_work_symmetry_operator = dictionary["chain_ids_to_work_symmetry_operator"]
+    # Because of chain name/id of PDB file must be A~Z and a~z,
+    # We can only create biological assembly that consists of less than 26 * 2 = 52 chains
     new_chain_ids_candicate = [
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        ]
+        *[chr(i) for i in range(65,91)], # A to Z
+        *[chr(i) for i in range(97,123)], # a to z
+    ]
+    # Existing ids
     chain_ids = [chain.get_id() for chain in chains]
+    # ids can be used for new chain.
+    # id must be unique.
     new_chain_ids = list(x for x in new_chain_ids_candicate if x not in chain_ids)
+
     for chain in chains:
         if chain.get_id() in chain_ids_to_work_symmetry_operator:
             for operator in dictionary["symmetry_operator"]:
-                # Unexpectedly, We can perform 
+                """
+                Pay attention, please.
+                We can work operators like r' = Ar + b
+                Moreover, this is a chain, not atoms.
+                We can perform like this because we override Chain.__mul__() and Chain.__add__()
+
+                Both of them perform:
+                1. Copy original chain.
+                2. Work operator to coodinates of all atoms.
+                3. Return new chain.
+                Go /biopython/Bio/PDB/Chain.py if you are interested in this behaviour.
+
+                Thus, we can write elegant codes like this.
+                """
                 new_chain = chain * operator["matrix"] + operator["shift"]
                 new_chain._id = new_chain_ids.pop(0)
                 model.add(new_chain)
 
+    # Delete models[0] consists of only unit structure(chains).
     structure.detach_child(0)
+    # and insert new one.
     structure.add(model)
     return structure
 
