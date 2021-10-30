6d1aae8058a59151dbd260853907459e9d0d9b19
flat35hd99 <flat35hd99@gmail.com>
2021-10-30 09:17:18 +0900

    Add setup.py

---
+++ symmetryshift/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='SymmetryShift',
+      version='1.0',
+      description='Extention of biopython.PDB for assemble asymmetric unit',
+      author='flat35hd99',
+      author_email='flat35hd99@gmail.com',
+      url='https://github.com/jphacks/D_2110',
+      packages=['symmetryshift'],
+     )
