1321cf6a66ff2c53475d0ed4b4068f4e7966bd90
flat35hd99 <flat35hd99@gmail.com>
2021-10-30 21:56:06 +0900

    Update creater func to separate save feature

--- symmetryshift/symmetryshift/create_biological_structure_unit.py
+++ symmetryshift/symmetryshift/create_biological_structure_unit.py
@@ -3,11 +3,30 @@ import sys
 # https://github.com/jphacks/D_2110#quick-install-and-use-guide
 # or disable comment-out below  
 
+# import os
 # sys.path.append(os.path.join(os.path.dirname(__file__), "../../biopython"))
 
 from Bio.PDB import PDBParser, PDBList, PDBIO, parse_pdb_header
+from Bio.PDB.Structure import Structure
 
-def creater(pdb_id: str, output="out.pdb"):
+def save_structure(structure, output="out.pdb"):
+    """ Save structure as PDB file
+    :param structure: A protein structure
+    :type structure: L{Structure}
+
+    :param output: Output filename or path
+    :type output: L{str}
+    """
+    io = PDBIO()
+    io.set_structure(structure)
+    io.save(output)
+
+def creater(pdb_id: str):
+    """ Create biological assembly as a structure
+
+    :param pdb_id: A PDB ID
+    :type pdb_id: L{str}
+    """
     # Fetch PDB file
     pdb = PDBList()
     pdb_file = pdb.retrieve_pdb_file(pdb_id, file_format="pdb")
@@ -33,25 +52,24 @@ def creater(pdb_id: str, output="out.pdb"):
     for chain in chains:
         if chain.get_id() in chain_ids_to_work_symmetry_operator:
             for operator in dictionary["symmetry_operator"]:
+                # Unexpectedly, We can perform 
                 new_chain = chain * operator["matrix"] + operator["shift"]
                 new_chain._id = new_chain_ids.pop(0)
                 model.add(new_chain)
 
     structure.detach_child(0)
     structure.add(model)
-
-    # Save new structure as PDB file
-    io = PDBIO()
-    io.set_structure(structure)
-    io.save(output)
+    return structure
 
 def main():
     pdb_id = sys.argv[1]
     if len(sys.argv) >= 3:
         output_filename = sys.argv[2]
-        creater(pdb_id=pdb_id, output=output_filename)
+        structure = creater(pdb_id=pdb_id)
+        save_structure(structure, output=output_filename)
     else:
-        creater(pdb_id=pdb_id)
+        structure = creater(pdb_id=pdb_id)
+        save_structure(structure)
 
 if __name__ == "__main__":
     main()
