83dfa5f5d3d4ac9848554b0755d55a2891300956
flat35hd99 <flat35hd99@gmail.com>
2021-11-05 21:34:16 +0900

    Update packaging flow

--- symmetryshift/setup.py
+++ symmetryshift/setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 import versioneer
 
+def get_long_description():
+      return ""
+
 setup(
       name="symmetryshift",
       version=versioneer.get_version(),
+      author="flat35hd99",
+      url="https://jphacks.github.io/D_2110/docs/home/",
       cmdclass=versioneer.get_cmdclass(),
       description="Create biological strucre unit from single PDB file",
-      author="flat35hd99",
+      long_description=get_long_description(),
+      download_url="https://github.com/jphacks/D_2110/",
       packages=find_packages(),
       entry_points={
             "console_scripts": [
