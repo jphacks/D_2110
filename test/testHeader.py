ae136c528f6dfa30214e78265797219b974a6382
flat35hd99 <38929316+flat35hd99@users.noreply.github.com>
2021-11-10 11:20:50 +0900

    Update /test

--- test/testHeader.py
+++ test/testHeader.py
@@ -1,11 +1,11 @@
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../biopython'))
+# import os, sys
+# sys.path.append(os.path.join(os.path.dirname(__file__), '../biopython'))
 
-from Bio.PDB import PDBParser, PDBList, parse_pdb_header
+# from Bio.PDB import PDBParser, PDBList, parse_pdb_header
 
-pdb_code="1KZU"
-pdb = PDBList()
-pdb_filename = pdb.retrieve_pdb_file(pdb_code, file_format="pdb")
-dictionary = parse_pdb_header(pdb_filename)
+# pdb_code="1KZU"
+# pdb = PDBList()
+# pdb_filename = pdb.retrieve_pdb_file(pdb_code, file_format="pdb")
+# dictionary = parse_pdb_header(pdb_filename)
 
-print(dictionary["symmetry_operator"])
+# print(dictionary["symmetry_operator"])
